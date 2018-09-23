dc521db865e24168409a477ed6492a38e5619900
Goodluck Focus <goodluck.focus@users.noreply.github.com>
2018-09-23 17:32:53 +0300

    fuqua v0.0.1 Focus Commit Changed and Added Propeties Titles in the Data Entry Activity
    v0.0.2 Focus Commit
    Coded the Login Screen layout
    Still awaiting the logo
    Colour Choice & design will need a review in the future

--- app/src/main/java/com/nyayozangu/labs/fuqua/activities/LoginActivity.java
+++ app/src/main/java/com/nyayozangu/labs/fuqua/activities/LoginActivity.java
@@ -2,14 +2,24 @@ package com.nyayozangu.labs.fuqua.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.nyayozangu.labs.fuqua.R;
 
 public class LoginActivity extends AppCompatActivity {
+    Button Login;
+    EditText Employee_id, password;
+    TextView forgot_password, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
+
+
 }
