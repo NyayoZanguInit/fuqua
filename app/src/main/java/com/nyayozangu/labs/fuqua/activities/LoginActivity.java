6dd5b199ad6bd23f4a996affe606244756e8f0ab
Goodluck Focus <goodluck.focus@users.noreply.github.com>
2018-09-23 17:52:59 +0300

    fuqua v0.0.1 Focus Commit Changed and Added Propeties Titles in the Data Entry Activity
    v0.0.2 Focus Commit
    Coded the Login Screen layout
    Still awaiting the logo
    Colour Choice & design will need a review in the future

--- app/src/main/java/com/nyayozangu/labs/fuqua/activities/LoginActivity.java
+++ app/src/main/java/com/nyayozangu/labs/fuqua/activities/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.nyayozangu.labs.fuqua.activities;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,5 +22,9 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    public void login(View view){
+        Intent startMain =new Intent(this, MainActivity.class);
+        startActivity(startMain);
+    }
 
 }
--- app/src/main/java/com/nyayozangu/labs/fuqua/activities/MainActivity.java
+++ app/src/main/java/com/nyayozangu/labs/fuqua/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent= getIntent();
 
         //initiate items
         ViewPager homeViewPager = findViewById(R.id.homeViewPager);
